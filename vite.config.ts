8a426e931475c4f5ea8381429fae7a7df6e3250f
Grace Zheng <grace.z@ikigai.team>
2026-01-26 16:20:53 +0800

    feat: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,10 @@ import tailwindcss from '@tailwindcss/vite'
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig, loadEnv } from 'vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
-// https://vite.dev/config/
-export default defineConfig(({ mode }) => {
+export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
 
   return {
@@ -50,11 +50,19 @@ export default defineConfig(({ mode }) => {
           '@libmedia/cheap/thread/atomics',
         ],
       }),
+      viteStaticCopy({
+        targets: [
+          {
+            src: 'node_modules/@libmedia/avplayer/dist/esm/[0-9]*.avplayer.js',
+            dest:
+              command === 'serve' ? './node_modules/.vite/deps/' : './assets/',
+          },
+        ],
+      }),
     ],
     resolve: {
       alias: {
         '@': fileURLToPath(new URL('./src', import.meta.url)),
-        // '@': path.resolve(__dirname, './src'),
       },
     },
     server: {
